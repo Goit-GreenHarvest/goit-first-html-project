0a13d5f4d635a34b7743e6e2ae63317d01010f9f
Mustafa Yeşildağ <kantik_385@hotmail.com>
2026-02-04 18:46:47 +0300

    proje son yapi duzenlendi

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import SortCss from 'postcss-sort-media-queries';
 
 export default defineConfig(({ command }) => {
   return {
+    base: '/goit-first-html-project/',
     define: {
       [command === 'serve' ? 'global' : '_global']: {},
     },
